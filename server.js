5301f71ba74a80df94d944cf9a6eb3c916a6d2e4
Jyoti Singh <kjyoti150205email@gmail.com>
2026-09-23 23:15:16 +0530

    Add trust proxy and update mobile web app meta tags

--- server.js
+++ server.js
@@ -94,6 +94,7 @@ const JWT_EXPIRY = '8h';
 
 // ================== EXPRESS ==================
 const app = express();
+app.set('trust proxy', 1);
 const port = process.env.PORT || 3000;
 
 const storage = multer.diskStorage({
